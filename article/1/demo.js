7ba005054ebf6cb6ae24252eda572053e8ba5cf9
HanRui <597589373@qq.com>
2019-01-15 11:56:46 +0800

    1

---
+++ article/1/demo.js
@@ -0,0 +1,19 @@
+function Os_loop(task) {
+
+  task()
+  setTimeout(setTimeout => {
+    function x()
+  },1000)
+}
+
+var task1 = () => {
+  console.log(('hello'));
+}
+
+var queue = []
+var i = 0
+
+for (let [key,value] of queue.entries()) {
+  
+}
+
---
+++ article/1/demo1.ts
@@ -0,0 +1,17 @@
+interface EventEmitter{
+  evens:{
+  [name:string]:Array<(args:Array<any>)=>void>
+  }
+}
+
+//添加、注册一个事件（名称-灰调函数）
+
+
+
+var a = new Map
+//检查是否有对应的Key
+if (a.has('name1')) {
+  a.set('key','value')
+}
+
+
